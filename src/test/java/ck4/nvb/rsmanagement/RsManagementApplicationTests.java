43140e2fd66a7ca73e7db83314ea319151868687
myrdin44 <nv.bac44@gmail.com>
2025-07-03 21:51:49 +0700

    first commit

---
+++ src/test/java/ck4/nvb/rsmanagement/RsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ck4.nvb.rsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
